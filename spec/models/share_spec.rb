3bea307591ffbfbf25597ee27771a16fcda87c94
wilDAlex <some@wild-x.ru>
2013-09-15 21:59:16 +0400

    Add some pending tests

---
+++ spec/models/share_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Share do
+  it "has a valid factory"
+  it "is invalid without file"
+  it "changed original filename"
+  it "saves original filename in database"
+end
