4c161bb35e39babc34fcec2a889a1c81fb286572
wilDAlex <some@wild-x.ru>
2013-09-23 19:35:38 +0400

    Testing GET methods in Show controller

--- spec/controllers/shares_controller_spec.rb
+++ spec/controllers/shares_controller_spec.rb
@@ -2,13 +2,31 @@ require 'spec_helper'
 
 describe SharesController do
 
+  before :each do
+    @user = FactoryGirl.create(:valid_user)
+    sign_in @user
+    @share = FactoryGirl.create(:share, user: @user)
+  end
+
   describe "GET #index" do
     it "populates an array of shares" do
-      user = FactoryGirl.create(:valid_user)
-      sign_in user
-      share = FactoryGirl.create(:share, user: user)
       get :index
-      assigns(:shares).should eq([share])
+      assigns(:shares).should eq([@share])
+    end
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested share to @share" do
+      get :show, id: @share
+      assigns(:share).should eq(@share)
+    end
+    it "renders the #show view" do
+      get :show, id: @share
+      response.should render_template :show
     end
   end
 
