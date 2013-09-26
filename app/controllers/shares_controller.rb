1dd6b11155a29fc3aca02ebf16b6884d5d0bfa7a
wilDAlex <some@wild-x.ru>
2013-09-26 23:52:44 +0400

    Show alert after deleting shares

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -58,7 +58,7 @@ class SharesController < ApplicationController
   def destroy
     @share.destroy
     respond_to do |format|
-      format.html { redirect_to shares_url }
+      format.html { redirect_to shares_url, alert: 'Share was successfully deleted.' }
       format.json { head :no_content }
     end
   end
