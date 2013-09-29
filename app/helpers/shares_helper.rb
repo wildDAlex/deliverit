129061c149bbf47c4f4c58ee9a7e96eb6b470580
wilDAlex <some@wild-x.ru>
2013-09-29 17:02:00 +0400

    Links on show action

--- app/helpers/shares_helper.rb
+++ app/helpers/shares_helper.rb
@@ -2,4 +2,9 @@ module SharesHelper
   def download_share_link(share)
     "/download/#{share.file.to_s.split('/').last}"
   end
+
+  def download_share_absolute_link(share)
+    port = request.port ? ":"+request.port.to_s : nil
+    "http://#{request.host}#{port}/download/#{share.file.to_s.split('/').last}"
+  end
 end
