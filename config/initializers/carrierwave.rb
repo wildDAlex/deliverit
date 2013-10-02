93e7c781ba7fa62ac837486f48ba631ba5353b99
wilDAlex <some@wild-x.ru>
2013-10-02 22:38:07 +0400

    Enabling processing for Test and Cucumber environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 if Rails.env.test? or Rails.env.cucumber?
   CarrierWave.configure do |config|
     config.storage = :file           # :fog for Amazon S3
-    config.enable_processing = false
+    config.enable_processing = true
 
     #Uncomment for Amazon S3 storage
     #config.fog_credentials = {
