8533f066257f96dd2f49fed63be680dd7d666f04
wilDAlex <some@wild-x.ru>
2013-10-02 22:59:52 +0400

    Take out links on share page to helper

--- app/helpers/shares_helper.rb
+++ app/helpers/shares_helper.rb
@@ -3,8 +3,24 @@ module SharesHelper
     "/download/#{version}#{"/" if !version.nil?}#{share.file.to_s.split('/').last}"
   end
 
-  def download_share_absolute_link(share)
+  def download_share_absolute_link(share, version = nil)
     port = request.port ? ":"+request.port.to_s : nil
-    "http://#{request.host}#{port}/download/#{share.file.to_s.split('/').last}"
+    "http://#{request.host}#{port}/download/#{version}#{"/" if !version.nil?}#{share.file.to_s.split('/').last}"
+  end
+
+  def image_in_img_tag(share)
+    "[IMG]#{download_share_absolute_link(share)}[/IMG]"
+  end
+
+  def preview_with_link(share)
+    "[URL=#{download_share_absolute_link(share)}][IMG]#{download_share_absolute_link(share, :medium)}[/IMG][/URL]"
+  end
+
+  def html_inline_link(share)
+    "<a target='_blank' href=#{share_url(share)}><img src=#{download_share_absolute_link(share)} border='0'></a>"
+  end
+
+  def html_inline_with_full_link(share)
+    "<a href='#{download_share_absolute_link(share)}' target='_blank'><img src='#{download_share_absolute_link(share, :medium)}' border='0'></a>"
   end
 end
