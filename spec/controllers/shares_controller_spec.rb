e32b71a2feb9c521d6f2bc96241cea8fdb5f3a15
wilDAlex <some@wild-x.ru>
2013-10-04 22:46:17 +0400

    Testing authorizing on Shares controller actions

--- spec/controllers/shares_controller_spec.rb
+++ spec/controllers/shares_controller_spec.rb
@@ -6,18 +6,37 @@ describe SharesController do
   before :each do
     DatabaseCleaner.clean
     @user = FactoryGirl.create(:valid_user)
+    @user2 = FactoryGirl.create(:valid_user)
     sign_in @user
-    @share = FactoryGirl.create(:share, user: @user)
+    @share = FactoryGirl.create(:share, user: @user, public: false)
+    @public_share = FactoryGirl.create(:share, user: @user, public: true)
   end
 
   describe "GET #index" do
     it "populates an array of shares" do
       get :index
-      assigns(:shares).should eq([@share])
+      assigns(:shares).should eq([@public_share, @share])
     end
     it "renders the :index view" do
       get :index
       response.should render_template :index
+    end
+
+    context "by not owner" do
+      it "not show another user shares in index" do
+        sign_out @user
+        sign_in @user2
+        get :index
+        assigns(:shares).should_not eq([@share])
+      end
+    end
+
+    context "by guest user" do
+      it "not show shares index" do
+        sign_out @user
+        get :index
+        response.should_not render_template :index
+      end
     end
   end
 
@@ -30,13 +49,43 @@ describe SharesController do
       get :show, id: @share
       response.should render_template :show
     end
+
+    context "by not owner" do
+      it "not show another user share if not public" do
+        sign_out @user
+        sign_in @user2
+        get :show, id: @share
+        response.should redirect_to root_url
+      end
+
+      it "show another user share if public" do
+        sign_out @user
+        sign_in @user2
+        get :show, id: @public_share
+        response.should render_template :show
+      end
+    end
+
+    context "by guest user" do
+      it "not show if not public" do
+        sign_out @user
+        get :show, id: @share
+        response.should redirect_to root_url
+      end
+      it "show if public" do
+        sign_out @user
+        get :show, id: @public_share
+        response.should render_template :show
+      end
+    end
+
   end
 
   describe "create" do
     context "with valid attributes" do
       it "creates a new share" do
         expect{
-          Share.create(file: @share.file, user: @user)
+          Share.create(file: @share.file, user: @user, public: false)
         }.to change(Share, :count).by(1)
       end
     end
@@ -47,7 +96,7 @@ describe SharesController do
   #    it "creates a new share" do
   #      expect{
   #        post :create, share: FactoryGirl.attributes_for(:share)
-  #      }.to change(Share,:count).by(1)
+  #      }.to change(Share, :count).by(1)
   #    end
   #  end
   #end
@@ -70,6 +119,56 @@ describe SharesController do
         response.should redirect_to @share
       end
     end
+
+    context "by not owner" do
+      it "not located the requested @share" do
+        sign_out @user
+        sign_in @user2
+        put :update, id: @share, share: FactoryGirl.attributes_for(:share)
+        response.should redirect_to root_url
+        put :update, id: @public_share, share: FactoryGirl.attributes_for(:share)
+        response.should redirect_to root_url
+      end
+
+      it "not changes @shares's attributes" do
+        sign_out @user
+        sign_in @user2
+        put :update, id: @share, share: FactoryGirl.attributes_for(:share, original_filename: "123.jpg", public: true)
+        @share.reload
+        @share.original_filename.should eq("test_image.jpg")
+        @share.public.should be_false
+
+        put :update, id: @public_share, share: FactoryGirl.attributes_for(:share, original_filename: "123.jpg", public: false)
+        @public_share.reload
+        @public_share.original_filename.should eq("test_image.jpg")
+        @public_share.public.should be_true
+      end
+    end
+
+    context "by guest user" do
+      it "not located the requested @share" do
+        sign_out @user
+        put :update, id: @share, share: FactoryGirl.attributes_for(:share)
+        assigns(:share).should_not eq(@share)
+
+        put :update, id: @public_share, share: FactoryGirl.attributes_for(:share)
+        assigns(:share).should_not eq(@share)
+      end
+
+      it "not changes @shares's attributes" do
+        sign_out @user
+        put :update, id: @share, share: FactoryGirl.attributes_for(:share, original_filename: "123.jpg", public: true)
+        @share.reload
+        @share.original_filename.should eq("test_image.jpg")
+        @share.public.should be_false
+
+        put :update, id: @public_share, share: FactoryGirl.attributes_for(:share, original_filename: "123.jpg", public: false)
+        @public_share.reload
+        @public_share.original_filename.should eq("test_image.jpg")
+        @public_share.public.should be_true
+      end
+    end
+
   end
 
   describe 'DELETE destroy' do
@@ -84,26 +183,32 @@ describe SharesController do
       response.should redirect_to shares_url
     end
 
-    it "allow only own share to be deleted" do
-      sign_out @user
-      @user2 = FactoryGirl.create(:valid_user)
-      sign_in @user2
-      expect{
-        delete :destroy, id: @share
-      }.to change(Share, :count).by(0)
-    end
-  end
+    context "by not owner" do
+      it "not deletes the share" do
+        sign_out @user
+        sign_in @user2
+        expect{
+          delete :destroy, id: @share
+        }.to change(Share, :count).by(0)
 
-  describe 'Access to anothers private share' do
-    it "redirects to index" do
-      sign_out @user
-      @user2 = FactoryGirl.create(:valid_user)
-      sign_in @user2
-      get :show, id: @share
-      response.should redirect_to root_url
-      #page.should have_content 'You are not authorized to access this page.'
+        expect{
+          delete :destroy, id: @public_share
+        }.to change(Share, :count).by(0)
+      end
     end
 
-  end
+    context "by guest user" do
+      it "not deletes the share" do
+        sign_out @user
+        expect{
+          delete :destroy, id: @share
+        }.to change(Share, :count).by(0)
 
+        expect{
+          delete :destroy, id: @public_share
+        }.to change(Share, :count).by(0)
+      end
+    end
+
+  end
 end
